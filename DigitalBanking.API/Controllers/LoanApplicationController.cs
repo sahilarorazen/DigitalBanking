33b9ff9fb32774e0d1ac83d920359a478e03cb9a
Sahil Arora <sahil.arora@zensar.com>
2026-08-30 19:35:31 +0530

    updated test message

--- DigitalBanking.API/Controllers/LoanApplicationController.cs
+++ DigitalBanking.API/Controllers/LoanApplicationController.cs
@@ -19,7 +19,7 @@ public class LoanApplicationController(ILoanApplicationService _loanApplicationS
     [HttpGet("test")]
     public IActionResult Test()
     {
-        return Ok("Working Now 2");
+        return Ok("Working Now 3");
     }
 
     [HttpGet("health")]
